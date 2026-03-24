4b4031e6961c838699c90a9b066cda37d4631188
Jena Creator <jena.sarcastic.creator@gmail.com>
2026-03-24 14:49:50 +0200

    Improvements
    - Added logger, instead of print
    - Saving raw data too
    - Creating output folder automatically

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
+from pathlib import Path
 from extract import extract_all_quotes
 from transform import transform_quotes
 from load import load_to_csv, load_to_sqlite
+import logging 
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def main():
-    print("Starting ETL pipeline...")
+    output_dir = Path("output")
+    output_dir.mkdir(exist_ok=True)
+    
+    logging.info("Starting ETL pipeline...")
     
     raw_quotes = extract_all_quotes()
-    print(f"Extracted {len(raw_quotes)} quotes.")
+    logging.info(f"Extracted {len(raw_quotes)} quotes.")
+    
+    with open(output_dir / "raw_quotes.json", "w", encoding="utf-8") as f:
+        import json
+        json.dump(raw_quotes, f, ensure_ascii=False, indent=2)
+    logging.info("Raw quotes saved to JSON.")
     
     transformed_quotes = transform_quotes(raw_quotes)
-    print(f"Transformed {len(transformed_quotes)} unique quotes.")
+    logging.info(f"Transformed {len(transformed_quotes)} unique quotes.")
     
-    load_to_csv(transformed_quotes)
-    print("Quotes loaded to CSV.")
+    load_to_csv(transformed_quotes, output_dir/"quotes.csv")
+    logging.info("Quotes loaded to CSV.")
     
-    load_to_sqlite(transformed_quotes, "quotes.db")
-    print("Quotes loaded to SQLite database.")
+    load_to_sqlite(transformed_quotes, output_dir/"quotes.db")
+    logging.info("Quotes loaded to SQLite database.")
     
-    print("ETL pipeline completed successfully.")
+    logging.info("ETL pipeline completed successfully.")
     
 if(__name__ == "__main__"):
     main()
